06d8f22457fa4183afc76bc8aa793f2d23278781
velvetweatly <21206898+velvetweatly@users.noreply.github.com>
2021-01-22 15:10:00 +0700

    build: weak key detection

--- src/chainwarden/pemread.py
+++ src/chainwarden/pemread.py
@@ -5,3 +5,6 @@ A PEM block looks like:
     -----BEGIN CERTIFICATE-----
     <base64>
     -----END CERTIFICATE-----
+
+We only decode CERTIFICATE blocks. Other labels (keys, CSRs) are skipped so a
+bundle that mixes material does not crash the reader.
