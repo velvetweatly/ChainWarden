d5d71088a0d4d01a33a1e6443cba1b5a3949f11c
velvetweatly <21206898+velvetweatly@users.noreply.github.com>
2021-06-16 17:12:00 +0700

    docs: expand weak key detection

---
+++ src/chainwarden/cli.py
@@ -0,0 +1,17 @@
+"""Command line interface for chainwarden.
+
+Subcommands:
+  audit    run all policy checks and print findings
+  chain    build and print candidate trust chains
+  expiry   list certificates ordered by expiry
+  version  print the version
+
+Input is one or more paths. A path may be a directory (every *.pem, *.crt and
+*.cer file inside is read, non recursively) or a single PEM file that may hold
+one or many CERTIFICATE blocks.
+
+The reference date is supplied with --as-of YYYY-MM-DD. It is required for the
+audit and expiry subcommands so output never depends on the wall clock, which
+keeps runs reproducible. The date is echoed in the output header.
+
+Exit codes: 0 clean, 1 findings present, 2 usage error.
