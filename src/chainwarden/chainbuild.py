f2a928fdc6e7b4c5e2ce78c4d4edd3c9adf3a71e
velvetweatly <21206898+velvetweatly@users.noreply.github.com>
2021-08-05 13:27:00 +0700

    style: clean up bundle parsing

---
+++ src/chainwarden/chainbuild.py
@@ -0,0 +1,10 @@
+"""Chain assembly by issuer to subject name matching.
+
+This is name based path building, not cryptographic verification. Given a pool
+of parsed certificates, for each leaf (a certificate that is not an issuer of
+any other certificate and is not self signed) we walk upward: repeatedly find a
+certificate whose subject equals the current certificate's issuer, until we
+reach a self signed certificate (a root) or run out of candidates.
+
+Ambiguity is possible when two certificates share a subject name. We record the
+first match in deterministic order and flag nothing here; policy decides
