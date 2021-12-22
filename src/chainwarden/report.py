2d9fafed810f0b47c2176a3aa03fa4e9478b3a3d
velvetweatly <21206898+velvetweatly@users.noreply.github.com>
2021-12-22 17:24:00 +0700

    perf: improve as-of reference date

---
+++ src/chainwarden/report.py
@@ -0,0 +1,6 @@
+"""Line oriented rendering. Every function returns a list of text lines so the
+CLI can join them and callers can diff them cleanly."""
+
+from __future__ import annotations
+
+from datetime import date
