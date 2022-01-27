73c4e5605de2ceede2b8fecdd0aa95cbca9e5c84
velvetweatly <21206898+velvetweatly@users.noreply.github.com>
2022-01-27 16:08:00 +0700

    feat: add docs index

--- tests/test_certmodel.py
+++ tests/test_certmodel.py
@@ -9,3 +9,9 @@ SAMPLES = Path(__file__).resolve().parent.parent / "samples"
 
 def _load(name: str):
     ders = certificate_ders((SAMPLES / name).read_text())
+    assert len(ders) == 1, name
+    return parse_certificate(ders[0], source=name)
+
+
+class TestCertModel(unittest.TestCase):
+    def test_root_is_self_signed_ca(self):
