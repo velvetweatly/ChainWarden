b2da7249a0d6877a37e82983a4f296c372548c17
velvetweatly <21206898+velvetweatly@users.noreply.github.com>
2022-01-27 16:03:00 +0700

    chore: bump weak key detection

--- tests/test_certmodel.py
+++ tests/test_certmodel.py
@@ -4,3 +4,8 @@ from pathlib import Path
 from chainwarden.certmodel import parse_certificate
 from chainwarden.pemread import certificate_ders
 
+SAMPLES = Path(__file__).resolve().parent.parent / "samples"
+
+
+def _load(name: str):
+    ders = certificate_ders((SAMPLES / name).read_text())
