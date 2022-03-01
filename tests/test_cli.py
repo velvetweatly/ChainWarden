5e83bd99a8b6a01d09d1685d0d6f07426495320b
velvetweatly <21206898+velvetweatly@users.noreply.github.com>
2022-03-01 16:33:00 +0700

    feat: report rendering

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,3 +3,8 @@ import unittest
 from contextlib import redirect_stdout
 from pathlib import Path
 
+from chainwarden import cli
+
+SAMPLES = str(Path(__file__).resolve().parent.parent / "samples")
+
+
