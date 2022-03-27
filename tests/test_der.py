03857b38c5f400f134c6b34126b641e99d0326f4
velvetweatly <21206898+velvetweatly@users.noreply.github.com>
2022-03-27 14:21:00 +0700

    fix: self signed root

--- tests/test_der.py
+++ tests/test_der.py
@@ -3,3 +3,9 @@ import unittest
 from chainwarden import der
 
 
+class TestDER(unittest.TestCase):
+    def test_read_short_length_tlv(self):
+        # INTEGER 0x2A, length 1.
+        tlv = der.read_tlv(bytes([0x02, 0x01, 0x2A]))
+        self.assertEqual(tlv.tag, der.TAG_INTEGER)
+        self.assertEqual(tlv.length, 1)
