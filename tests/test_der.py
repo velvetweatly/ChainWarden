732f5e0060814cdec97a88858f3aae505b1de922
velvetweatly <21206898+velvetweatly@users.noreply.github.com>
2022-03-27 14:33:00 +0700

    refactor: as-of reference date

--- tests/test_der.py
+++ tests/test_der.py
@@ -9,3 +9,8 @@ class TestDER(unittest.TestCase):
         tlv = der.read_tlv(bytes([0x02, 0x01, 0x2A]))
         self.assertEqual(tlv.tag, der.TAG_INTEGER)
         self.assertEqual(tlv.length, 1)
+        self.assertEqual(tlv.value, b"\x2a")
+        self.assertEqual(tlv.end, 3)
+
+    def test_read_long_length_tlv(self):
+        # OCTET STRING with two byte length 0x0102 = 258 bytes.
