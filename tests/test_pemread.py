4a73f9d8ed4d31edca249d9ce25cfbb812db6b61
velvetweatly <21206898+velvetweatly@users.noreply.github.com>
2022-03-31 13:43:00 +0700

    fix: subject alt names

--- tests/test_pemread.py
+++ tests/test_pemread.py
@@ -2,3 +2,8 @@ import base64
 import unittest
 
 from chainwarden import pemread
+
+
+def _wrap(label: str, data: bytes) -> str:
+    b64 = base64.b64encode(data).decode("ascii")
+    lines = [b64[i : i + 64] for i in range(0, len(b64), 64)]
