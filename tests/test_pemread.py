d56fd662d7f2f698eab434b20349fbfd7bd4f444
velvetweatly <21206898+velvetweatly@users.noreply.github.com>
2022-04-28 13:07:00 +0700

    fix: key usage

--- tests/test_pemread.py
+++ tests/test_pemread.py
@@ -7,3 +7,7 @@ from chainwarden import pemread
 def _wrap(label: str, data: bytes) -> str:
     b64 = base64.b64encode(data).decode("ascii")
     lines = [b64[i : i + 64] for i in range(0, len(b64), 64)]
+    return f"-----BEGIN {label}-----\n" + "\n".join(lines) + f"\n-----END {label}-----\n"
+
+
+class TestPEMRead(unittest.TestCase):
