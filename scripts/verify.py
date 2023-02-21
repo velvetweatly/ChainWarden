23a3a99d7ad0554ba7a5fcf1f106416aa2af5827
velvetweatly <21206898+velvetweatly@users.noreply.github.com>
2023-02-21 15:07:00 +0700

    chore: bump der tag length value walker

---
+++ scripts/verify.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""ChainWarden quality gate.
+
+Standard library only. Run from the project root:
+
+    python scripts/verify.py
+
+Exit code is 0 when every check passes and 1 when any check fails. One line is
+printed per check, followed by a summary line. The checks encode the lessons in
+_standards/LESSONS.md that can be verified mechanically:
+
+  1. Every .svg under docs/assets/ parses as XML.
+  2. No .svg contains feGaussianBlur, feDropShadow, or feTurbulence.
+  3. No XML comment in any .svg contains the illegal `--` sequence.
+  4. No tracked text file contains the em dash character U+2014 or its numeric
+     or named HTML entity forms.
+  5. README.md contains no pandoc style image attribute block.
+  6. README.md contains none of the banned marketing terms.
+  7. Every .svg under docs/assets/ carries a viewBox, role="img", a <title>,
+     and a <desc>.
+  8. No two text labels sharing a baseline in any .svg overlap.
+"""
+
+from __future__ import annotations
+
+import re
+import sys
+import xml.etree.ElementTree as ET
+from pathlib import Path
